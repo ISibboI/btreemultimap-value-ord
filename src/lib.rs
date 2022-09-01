c76dd90e86d06f54eefd1b2b01896d8e17950d0c
Sebastian Schmidt <isibboi@gmail.com>
2022-09-01 18:31:12 +0300

    Fix for serde.

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,12 @@ use std::collections::BTreeMap;
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub struct BTreeMultiMap<Key, Value> {
+pub struct BTreeMultiMap<Key: Ord, Value: Ord> {
     map: BTreeMap<Key, BTreeMap<Value, usize>>,
 }
 
-impl<Key, Value> BTreeMultiMap<Key, Value> {
-    pub fn new() -> Self
-    where
-        Key: Default,
-        Value: Default,
-    {
+impl<Key: Ord, Value: Ord> BTreeMultiMap<Key, Value> {
+    pub fn new() -> Self {
         Default::default()
     }
 
@@ -21,11 +17,7 @@ impl<Key, Value> BTreeMultiMap<Key, Value> {
         self.map.clear()
     }
 
-    pub fn insert(&mut self, key: Key, value: Value)
-    where
-        Key: Ord,
-        Value: Ord,
-    {
+    pub fn insert(&mut self, key: Key, value: Value) {
         if let Some(map) = self.map.get_mut(&key) {
             if let Some(count) = map.get_mut(&value) {
                 *count += 1;
@@ -37,18 +29,11 @@ impl<Key, Value> BTreeMultiMap<Key, Value> {
         }
     }
 
-    pub fn get(&self, key: &Key) -> Option<&BTreeMap<Value, usize>>
-    where
-        Key: Ord,
-    {
+    pub fn get(&self, key: &Key) -> Option<&BTreeMap<Value, usize>> {
         self.map.get(key)
     }
 
-    pub fn get_value_count(&self, key: &Key, value: &Value) -> usize
-    where
-        Key: Ord,
-        Value: Ord,
-    {
+    pub fn get_value_count(&self, key: &Key, value: &Value) -> usize {
         if let Some(map) = self.map.get(key) {
             map.get(value).copied().unwrap_or(0)
         } else {
@@ -56,18 +41,11 @@ impl<Key, Value> BTreeMultiMap<Key, Value> {
         }
     }
 
-    pub fn remove(&mut self, key: &Key) -> Option<BTreeMap<Value, usize>>
-    where
-        Key: Ord,
-    {
+    pub fn remove(&mut self, key: &Key) -> Option<BTreeMap<Value, usize>> {
         self.map.remove(key)
     }
 
-    pub fn remove_key_value(&mut self, key: &Key, value: &Value) -> bool
-    where
-        Key: Ord,
-        Value: Ord,
-    {
+    pub fn remove_key_value(&mut self, key: &Key, value: &Value) -> bool {
         if let Some(map) = self.map.get_mut(key) {
             if let Some(count) = map.get_mut(value) {
                 *count -= 1;
@@ -84,7 +62,7 @@ impl<Key, Value> BTreeMultiMap<Key, Value> {
     }
 }
 
-impl<Key, Value> Default for BTreeMultiMap<Key, Value> {
+impl<Key: Ord, Value: Ord> Default for BTreeMultiMap<Key, Value> {
     fn default() -> Self {
         Self {
             map: Default::default(),
@@ -92,11 +70,7 @@ impl<Key, Value> Default for BTreeMultiMap<Key, Value> {
     }
 }
 
-impl<Key, Value> FromIterator<(Key, Value)> for BTreeMultiMap<Key, Value>
-where
-    Key: Ord,
-    Value: Ord,
-{
+impl<Key: Ord, Value: Ord> FromIterator<(Key, Value)> for BTreeMultiMap<Key, Value> {
     fn from_iter<T: IntoIterator<Item = (Key, Value)>>(iter: T) -> Self {
         let mut result = Self::default();
         for (key, value) in iter {
